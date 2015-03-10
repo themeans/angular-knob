fcc08e1d8c76a414117f64b91f635116527759c0
amay0048 <anthonyjmay@gmail.com>
2015-03-11 00:53:30 +1100

    Update angular-knob.js

--- src/angular-knob.js
+++ src/angular-knob.js
@@ -1,31 +1,71 @@
-angular.module('ui.knob', []).directive('knob', ['$timeout', function($timeout) {
-    'use strict';
+'use strict';
+angular.module('ui.knob', []).directive('knob', ['$timeout', function() {
 
     return {
         restrict: 'EA',
         replace: true,
-        template: '<input value="{{ knobData }}"/>',
+        template: '<input autocomplete="off" value="{{ knobData }}"/>',
         scope: {
             knobData: '=',
-            knobOptions: '&'
+            knobOptions: '&',
+            knobRelease: '=',
+            knobChange: '='
         },
-        link: function($scope, $element) {
+        link: function(scope, element) {
+            scope.el = element;
+        },
+        controller: function($scope, $window){
             var knobInit = $scope.knobOptions() || {};
 
+            knobInit.change = function(newValue) {
+                if(newValue)
+                {
+                    $scope.$evalAsync(function(){
+                        $scope.knobData = Number(newValue).toFixed(0);
+                    });
+                }
+            };
+
             knobInit.release = function(newValue) {
-                $timeout(function() {
-                    $scope.knobData = newValue;
-                    $scope.$apply();
+                try
+                {
+                    $scope.knobRelease();
+                }
+                catch(e)
+                {
+                    debugger;
+                }
+                $scope.$evalAsync(function(){
+                    $scope.knobData = newValue;    
                 });
             };
 
             $scope.$watch('knobData', function(newValue, oldValue) {
-                if (newValue != oldValue) {
-                    $($element).val(newValue).change();
+                if (newValue !== oldValue)
+                {
+                    $window.jQuery($scope.el).val(newValue).change();
                 }
             });
 
-            $($element).val($scope.knobData).knob(knobInit);
+            var cancel = $scope.$watch('el',function(newVal){
+                if(newVal)
+                {
+                    var initVal = $scope.knobData || 0;
+
+                    $window
+                    .jQuery($scope.el)
+                    .knob(knobInit);
+
+                    $window
+                    .jQuery($scope.el)
+                    .val(initVal)
+                    .trigger('change');
+
+                    knobInit.change(initVal);
+
+                    cancel();
+                }
+            });
         }
     };
 }]);
